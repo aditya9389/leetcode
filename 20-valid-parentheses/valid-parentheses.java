6b800e19215c83c6f724e2b8e7a93fe889c4e1b0
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-02-19 01:31:53 +0530

    Time: 2 ms (58.68%) | Memory: 41.1 MB (91.40%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+//copied but understandable also learn stack in java to use this 
+
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<Character>();
+        for(char ch : s.toCharArray()){
+            if(stk.empty())
+                stk.push(ch);
+            else if(stk.peek() == '(' && ch == ')')
+                stk.pop();
+            else if(stk.peek() == '{' && ch == '}')
+                stk.pop();
+            else if(stk.peek() == '[' && ch == ']')
+                stk.pop();
+            else   
+                stk.push(ch);
+        }
+        if(stk.empty())
+            return true;
+        return false;
+    }
+}
