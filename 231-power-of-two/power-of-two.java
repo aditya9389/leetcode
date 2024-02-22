8c8ec9e3d11b79debe77aa393906f5635623ba29
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-02-23 03:28:48 +0530

    Time: 1 ms (46.58%) | Memory: 40.5 MB (91.16%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,12 +1,34 @@
-//power function with 0(1) cpomplexity
+//power function with 0(1) cpomplexity but noob
+// public class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         for (int i = 0; i < 31; i++) {
+//             int ans = (int) Math.pow(2, i);
+//             if (ans == n) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
 public class Solution {
     public boolean isPowerOfTwo(int n) {
-        for (int i = 0; i < 31; i++) {
-            int ans = (int) Math.pow(2, i);
-            if (ans == n) {
-                return true;
-            }
+        int x = 1;
+        while (x <= n) {
+            if (x == n) return true;
+            if (x > Integer.MAX_VALUE / 2) break;
+            x = x << 1;
         }
         return false;
     }
 }
+//shift operator gg
+
+// public class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         if (n == 0) return false;
+//         return Math.floor(Math.log(n) / Math.log(2))
+//          == Math.ceil(Math.log(n) / Math.log(2));
+//     }
+// }
+//log method  n=2^n -> log(n)=n(log2) incase of 2 multiple ceil and floor willl always be equal
