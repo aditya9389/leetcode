0534b55ce51b89ea6d2d02e967c71c865eb2be4c
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-02-22 12:52:54 +0530

    Time: 2 ms (99.85%) | Memory: 54.5 MB (6.10%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] trustcount = new int[n + 1];
+
+        for (int[] relation : trust) {
+            trustcount[relation[0]]--;
+            trustcount[relation[1]]++;
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            if (trustcount[i] == n - 1) {
+                return i;
+            }
+        }
+
+        return -1; // no judge found
+    }
+}
+//this  is copied one but i understood it good solution 
+
+// class Solution {
+//     public int findJudge(int n, int[][] trust) {
+//         int num=trust.length;
+//         int t=0;
+//         for(int[] temp: trust )
+//         {
+//             if(temp[1]==n)
+//             t++;
+//         }
+//         if(num==t)
+//         return n;
+//         return -1;
+//     }
+// }   
+//   ---->thought something like this because i thought we need to check for n. but n is
+//        the num ber of people
