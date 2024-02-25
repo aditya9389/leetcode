b65b09537914d7492256e77347bab1d6c234156d
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-02-25 06:24:55 +0530

    Time: 2 ms (39.86%) | Memory: 43.9 MB (66.89%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -29,3 +29,33 @@ class Solution {
 }
 
 //understandble and well done
+
+
+// class Solution {
+//     public int compress(char[] chars) {
+//         int count = 1;
+//         StringBuilder sb = new StringBuilder();
+//         sb.append(chars[0]);
+//         for(int i = 1; i < chars.length; i++){
+//             if(chars[i] == chars[i-1]){
+//                 count++;
+//             }
+//             else{
+//                 if(count > 1){
+//                     sb.append(count);
+//                 }
+//                     sb.append(chars[i]);
+//                     count = 1;
+//             }   
+//      }
+//      if(count > 1){
+//          sb.append(count);
+//      }
+
+//      for(int j = 0; j < sb.length(); j++){
+//          chars[j] = sb.charAt(j);
+//      }
+//      return sb.length();
+//    }
+// }
+//this one is copied but better solution..append works better in this condition
