d3a9b8b89d3afc3a6e47395c7050835e122caa66
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-02-27 16:29:13 +0530

    Time: 13 ms (33.42%) | Memory: 44.9 MB (9.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,32 +1,31 @@
 import java.util.Arrays;
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
-        
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
-        
-        return Arrays.equals(sChars, tChars);
+        if(s.length()!=t.length())
+        return false;
+        Map<Character,Integer> words =new HashMap<>();
+        for(char w : s.toCharArray())
+            words.put(w,words.getOrDefault(w,0)+1);
+        for(char w: t.toCharArray())
+            words.put(w,words.getOrDefault(w,0)-1);
+        for(int val:words.values())
+        if(val!=0)
+        return false;
+        return true;
     }
 }
-//above one is copied allthough easy just sorting in it 0(nlogn)
+
+// 0(logn) and done by own self
 
 // class Solution {
 //     public boolean isAnagram(String s, String t) {
-//         if(s.length()!=t.length())
-//         return false;
-//         Map<Character,Integer> words =new HashMap<>();
-//         for(char w : s.toCharArray())
-//             words.put(w,words.getOrDefault(w,0)+1);
-//         for(char w: t.toCharArray())
-//             words.put(w,words.getOrDefault(w,0)-1);
-//         for(int val:words.values())
-//         if(val!=0)
-//         return false;
-
-//         return true;
+//         char[] sChars = s.toCharArray();
+//         char[] tChars = t.toCharArray();
+        
+//         Arrays.sort(sChars);
+//         Arrays.sort(tChars);
+        
+//         return Arrays.equals(sChars, tChars);
 //     }
 // }
-// 0(logn) so we will use sorting which is better in this condition
+//above one is copied allthough easy just sorting in it 0(nlogn) 
