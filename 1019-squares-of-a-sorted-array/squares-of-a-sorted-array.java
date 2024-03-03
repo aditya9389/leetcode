ea10dc794f29ca83eb99317f1af796475ae14765
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-03-03 14:44:42 +0530

    Time: 8 ms (17.73%) | Memory: 46.8 MB (66.74%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,27 +1,41 @@
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         int n=nums.length;
+//         int[] ans= new int[n];
+//         int i=0,j=n-1;
+//         int count=n-1;
+//         while(i<=j)
+//         {
+//             int ip=(int)Math.pow(nums[i],2);
+//             int jp=(int)Math.pow(nums[j],2);
+//             if(ip<jp)
+//             {
+//                 ans[count]=jp;
+//                 j--;
+//                 count--;
+//             }
+//             else
+//             {
+//                 ans[count]=ip;
+//                 i++;
+//                 count--;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+//done by myself in 1st try 
+
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n=nums.length;
-        int[] ans= new int[n];
-        int i=0,j=n-1;
-        int count=n-1;
-        while(i<=j)
-        {
-            int ip=(int)Math.pow(nums[i],2);
-            int jp=(int)Math.pow(nums[j],2);
-            if(ip<jp)
-            {
-                ans[count]=jp;
-                j--;
-                count--;
-            }
-            else
-            {
-                ans[count]=ip;
-                i++;
-                count--;
-            }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
         }
-        return ans;
+        Arrays.sort(nums);
+        return nums;
     }
 }
-//done by myself in 1st try 
+//this is copied but better and nlogn time and 1 space
+
+
