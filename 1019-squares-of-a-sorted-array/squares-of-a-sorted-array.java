9454074ff57f349104651cfeb00cd1edbed8d835
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-03-03 14:45:03 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (61.45%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,41 +1,41 @@
-// class Solution {
-//     public int[] sortedSquares(int[] nums) {
-//         int n=nums.length;
-//         int[] ans= new int[n];
-//         int i=0,j=n-1;
-//         int count=n-1;
-//         while(i<=j)
-//         {
-//             int ip=(int)Math.pow(nums[i],2);
-//             int jp=(int)Math.pow(nums[j],2);
-//             if(ip<jp)
-//             {
-//                 ans[count]=jp;
-//                 j--;
-//                 count--;
-//             }
-//             else
-//             {
-//                 ans[count]=ip;
-//                 i++;
-//                 count--;
-//             }
-//         }
-//         return ans;
-//     }
-// }
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n=nums.length;
+        int[] ans= new int[n];
+        int i=0,j=n-1;
+        int count=n-1;
+        while(i<=j)
+        {
+            int ip=(int)Math.pow(nums[i],2);
+            int jp=(int)Math.pow(nums[j],2);
+            if(ip<jp)
+            {
+                ans[count]=jp;
+                j--;
+                count--;
+            }
+            else
+            {
+                ans[count]=ip;
+                i++;
+                count--;
+            }
+        }
+        return ans;
+    }
+}
 //done by myself in 1st try 
 
 
-class Solution {
-    public int[] sortedSquares(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = nums[i] * nums[i];
-        }
-        Arrays.sort(nums);
-        return nums;
-    }
-}
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         for (int i = 0; i < nums.length; i++) {
+//             nums[i] = nums[i] * nums[i];
+//         }
+//         Arrays.sort(nums);
+//         return nums;
+//     }
+// }
 //this is copied but better and nlogn time and 1 space
 
 
