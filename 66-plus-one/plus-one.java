691c7931787d8933ed1ba0676b9104bf9a4f4dda
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-03-06 02:02:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (34.21%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -7,8 +7,9 @@ class Solution {
 	    }
 	    digits[i] = 0;
     }
-    digits = new int[digits.length + 1];
+    digits = new int[digits.length + 1];    //we will come accorss this in only 999 cases
     digits[0] = 1;
     return digits;
     }
 }
+//thought diff this one is copeied but understood
