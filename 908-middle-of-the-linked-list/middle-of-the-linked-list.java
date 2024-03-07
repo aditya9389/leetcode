4f3041eec822969a8ed002e774a57acf8ae0414a
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-03-07 13:26:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (34.31%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -8,23 +8,37 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public static int len(ListNode head)
-    {
-        int l=1;
-        while(head.next!=null){
-        l++;
-        head=head.next;}
-        return l;
-    }
+ class Solution {
     public ListNode middleNode(ListNode head) {
-        if(head.next==null)
-        return head;
-        int len=len(head);
-        System.out.println(len);
-        for(int i=0;i<len/2;i++)
-        head= head.next;
-        return head;
-    }
+        ListNode fast = head;
+       ListNode slow = head;
+                        
+            while(fast!=null && fast.next!=null){    
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+        return slow;
+    }  
 }
+//this one is better too and i knew this one too
+// class Solution {
+//     public static int len(ListNode head)
+//     {
+//         int l=1;
+//         while(head.next!=null){
+//         l++;
+//         head=head.next;
+//         }
+//         return l;
+//     }
+//     public ListNode middleNode(ListNode head) {
+//         if(head.next==null)
+//         return head;
+//         int len=len(head);
+//         System.out.println(len);
+//         for(int i=0;i<len/2;i++)
+//         head= head.next;
+//         return head;
+//     }
+// }
 //self done very easy
