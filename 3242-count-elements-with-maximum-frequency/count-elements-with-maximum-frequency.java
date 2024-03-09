256cff5768bde5e43b3c63f912ea55f725c697c1
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-03-09 15:43:00 +0530

    Time: 2 ms (70.78%) | Memory: 42.4 MB (39.77%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -19,3 +19,28 @@ class Solution {
         return ans;
     }
 }
+//self created solution and logic didnt even looked for it. although lots of ans are on
+//same logic also this can be done with array which is kind of good 
+
+// class Solution {
+//     public int maxFrequencyElements(int[] nums) {
+//         int max=0;
+//         for(int i=0;i<nums.length;i++){
+//             max=Math.max(nums[i],max);
+//         }
+//         int B[]= new int[max+1];
+//         int temp=0;
+//         for(int i=0;i<nums.length;i++){
+//             B[nums[i]]++; 
+//             temp=Math.max(temp,B[nums[i]]);
+//         }
+//         int count=0;
+//         for(int i=0;i<max+1;i++){
+//             if(B[i]==temp){
+//                 count+=B[i];
+//             }
+//         }
+//         return count;
+//     }
+// }
+//this one is copy
