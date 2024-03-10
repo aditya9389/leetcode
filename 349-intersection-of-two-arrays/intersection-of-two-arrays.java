8ca716032bf879848793166d2f96ce6072b5fca2
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-03-10 12:32:07 +0530

    Time: 2 ms (94.84%) | Memory: 43.1 MB (72.75%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,26 +1,53 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        List<Integer> ans= new ArrayList<>();
-        int i=0,j=0;
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        while(i<nums1.length&&j<nums2.length)
-        {
-            if(nums1[i]==nums2[j]){
-                if(!ans.contains(nums1[i]))
-                ans.add(nums1[i]);
-                i++;
-                j++;
-                }
-            else if(nums1[i]>nums2[j])
-            j++;
-            else
-            i++;    
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=0;i<nums1.length;i++){
+            set.add(nums1[i]);
         }
-        int[] ansarr= new int[ans.size()];
-        for(int k=0;k<ans.size();k++)
-            ansarr[k]=ans.get(k);
-        return ansarr;
+
+        for(int i =0;i<nums2.length;i++){
+            if(set.contains(nums2[i])){
+                list.add(nums2[i]);
+            }
+            set.remove(nums2[i]);
+        }
+
+        int[] arr = new int[list.size()];
+
+        for(int i=0;i<arr.length;i++){
+            arr[i] = list.get(i);
+        }
+
+        return arr;
     }
 }
-//self done
+//copied but understood done without sorting simple 
+
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+//         List<Integer> ans= new ArrayList<>();
+//         int i=0,j=0;
+//         Arrays.sort(nums1);
+//         Arrays.sort(nums2);
+//         while(i<nums1.length&&j<nums2.length)
+//         {
+//             if(nums1[i]==nums2[j]){
+//                 if(!ans.contains(nums1[i]))
+//                 ans.add(nums1[i]);
+//                 i++;
+//                 j++;
+//                 }
+//             else if(nums1[i]>nums2[j])
+//             j++;
+//             else
+//             i++;    
+//         }
+//         int[] ansarr= new int[ans.size()];
+//         for(int k=0;k<ans.size();k++)
+//             ansarr[k]=ans.get(k);
+//         return ansarr;
+//     }
+// }
+//self done with sorting
