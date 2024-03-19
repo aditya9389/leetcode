ba6e5e1d599126a4569d3717a7926e5e0dfe64e0
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-03-20 00:00:49 +0530

    Time: 2 ms (99.12%) | Memory: 46.1 MB (20.79%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,17 +1,24 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int total= tasks.length;
-        int max=0;
-        int[] count= new int[26];
-        for(char ch:tasks)
-        count[ch-'A']+=1;
-        for(int c:count)    
-        max=max>c? max:c;
-        int countmax=0;
-        for(int m:count)
-        if(m==max)
-        countmax++;
-        return Math.max((n+1)*(max-1)+countmax,total);
+        if (n == 0) {
+                return tasks.length;
+            }
+            int[] f = new int[26];
+            for (char c : tasks) {
+                f[c - 'A']++;
+            }
+            int max = 0;
+            int count = 0;
+            for (int i : f) {
+                if (i > max) {
+                    max = i;
+                    count = 1;
+                } else if (i == max) {
+                    count++;
+                }
+
+            }
+            return Math.max(tasks.length, (max - 1) * (n + 1) + count);
     }
 }
 //learned the logic implemented myself 
