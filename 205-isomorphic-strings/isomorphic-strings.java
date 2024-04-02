a56fd883576e3158a0662041336699df499ca1d3
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-04-02 23:38:50 +0530

    Time: 9 ms (68.74%) | Memory: 42.5 MB (37.68%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         Map<Character,Character> map= new HashMap<>();
-        int i=0;
-        for(char c:s.toCharArray())
+        int i=0;        //use i for iterating over t
+        for(char c:s.toCharArray())     //run for each loop on s
         {
-            if(!map.containsKey(c))
-            if(!map.containsValue(t.charAt(i)))
-            map.put(c,t.charAt(i));
-            else
-            return false;
-            else
-            if(map.get(c)!=t.charAt(i))
-            return false;
-            i++;
+            if(!map.containsKey(c))           //check if it contains the new key or not
+            {                                       
+                if(!map.containsValue(t.charAt(i))) //then check if that value is there or not
+                    map.put(c,t.charAt(i));    //if both are not present then put them
+                else
+                    return false;           //if value is already there then directly return false
+            }                               
+            else if(map.get(c)!=t.charAt(i)) //if key is there already then check if its value 
+                return false;               //is similar to new value -- if not return false
+            
+            i++;                            //no mater what increase i
         }
-        return true;
+        return true;        //after loop direct exit
     }
 }
-//self done didnt even looked
