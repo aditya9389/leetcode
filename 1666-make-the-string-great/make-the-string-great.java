1639ec6f181a62440e42ad9be9028cef01ea75de
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-04-05 15:30:58 +0530

    Time: 3 ms (61.07%) | Memory: 42.1 MB (67.44%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && Math.abs(c - stack.peek()) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.insert(0, stack.pop());
+        }
+        
+        return result.toString();
+    }
+}
+//simple with stack ..same logic i was trying to do with only string builder..but cant
