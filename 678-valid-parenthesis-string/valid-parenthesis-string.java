80734f29bf59bd1d3f663402bc91a434991c4100
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-04-07 23:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (57.11%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean checkValidString(String s) {
+        int leftMin = 0, leftMax = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (c == ')') {
+                leftMin--;
+                leftMax--;
+            } else {
+                leftMin--;
+                leftMax++;
+            }
+            if (leftMax < 0) return false;
+            if (leftMin < 0) leftMin = 0;
+        }
+        
+        return leftMin == 0;
+    }
+}
+//didnt even read just did for streak
