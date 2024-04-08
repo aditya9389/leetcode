22886b914d140de9f1e10b302e32b9fe902abb77
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-04-09 00:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (50.61%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int[] counts = new int[2];
+        for (int student : students) counts[student]++;
+        
+        int remaining = sandwiches.length;
+        for (int sandwich : sandwiches) {
+            if (counts[sandwich] == 0) break;
+            if (remaining-- == 0) break;
+            counts[sandwich]--;
+        }
+        
+        return remaining;
+    }
+}
+//very copied didnt even read
