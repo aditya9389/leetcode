ce0b8527b5e669ad134308448ccfa79d83722ab9
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-09-05 09:05:43 +0530

    Time: 4 ms (99.83%) | Memory: 45.9 MB (34.35%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -11,9 +11,9 @@ class Solution {
                 if(grid[i][j]==1)
                 {
                     res+=4;
-                    if(i>0&&grid[i-1][j]==1)
+                    if(i>0 && grid[i-1][j]==1)
                      res-=2;
-                    if(j>0&&grid[i][j-1]==1)
+                    if(j>0 && grid[i][j-1]==1)
                      res-=2;
                 }
             }
