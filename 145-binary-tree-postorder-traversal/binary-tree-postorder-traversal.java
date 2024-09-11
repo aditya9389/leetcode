e47b72a80f7415f6c299b559f98af300ebd81461
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-09-11 13:44:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (19.49%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,30 +15,45 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-     Stack<TreeNode> st=new Stack<>();
+    //  Stack<TreeNode> st=new Stack<>();      //this one is for 1 stack 
+    //     List<Integer> res= new LinkedList<>();
+    //     if(root==null)
+    //     return res;
+    //     TreeNode curr= root;
+    //     TreeNode lastv=null;
+    //     while(!st.isEmpty()||curr!=null)
+    //     {
+    //         if(curr!=null)
+    //         {
+    //             st.push(curr);
+    //             curr=curr.left;
+    //         }
+    //         else
+    //         {
+    //             TreeNode peeknode= st.peek();
+    //             if(peeknode.right!=null&&lastv!=peeknode.right)
+    //             curr=(peeknode.right);
+    //             else{
+    //                     res.add(peeknode.val);
+    //                     lastv=st.pop();
+    //             }
+    //         }
+    //     }
+        Stack<TreeNode> st1=new Stack<>();      //this one is for 2 stack
+        Stack<TreeNode> st2= new Stack<>();
         List<Integer> res= new LinkedList<>();
         if(root==null)
         return res;
-        TreeNode curr= root;
-        TreeNode lastv=null;
-        while(!st.isEmpty()||curr!=null)
+        st1.push(root);
+        while(!st1.isEmpty())
         {
-            if(curr!=null)
-            {
-                st.push(curr);
-                curr=curr.left;
-            }
-            else
-            {
-                TreeNode peeknode= st.peek();
-                if(peeknode.right!=null&&lastv!=peeknode.right)
-                curr=(peeknode.right);
-                else{
-                        res.add(peeknode.val);
-                        lastv=st.pop();
-                }
-            }
+            TreeNode temp= st1.pop();
+            st2.push(temp);
+            if(temp.left!=null)st1.push(temp.left);
+            if(temp.right!=null)st1.push(temp.right);
         }
+        while(!st2.isEmpty())
+        res.add(st2.pop().val);
         //postorder(root,res);
         return res;
     }
