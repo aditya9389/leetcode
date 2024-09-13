f934ee730076417d97a9539d710142008c8dbe93
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2024-09-14 03:13:00 +0530

    Time: 12 ms (26.58%) | Memory: 56.3 MB (71.97%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,22 +1,43 @@
 class Solution {
+    private int[] tree;
+
     public int[] xorQueries(int[] arr, int[][] queries) {
-        int n=arr.length;
-        int[] pre=new int[n];
-        pre[0]=arr[0];
-        for(int i=1;i<n;i++) {
-            pre[i]=pre[i-1]^arr[i];
+        int n = arr.length;
+        int m = queries.length;
+        tree = new int[4 * n];
+        int[] ans = new int[m];
+        
+        build(0, n - 1, 0, arr);
+
+        for(int i=0;i<m;i++) {
+            ans[i] = query(queries[i][0], queries[i][1], 0, n - 1, 0);
         }
-        int[] res=new int[queries.length];
-        for(int k=0;k<queries.length;k++) {
-            int i=queries[k][0];
-            int j=queries[k][1];
-            if(i==0) {
-                res[k]=pre[j];
-            } else {
-                res[k]=pre[j]^pre[i-1];
-            }
+        return ans;
+    }
+
+    private void build(int ss, int se, int si, int[] nums) {
+        if(ss > se)
+            return;
+        if(ss == se) {
+            tree[si] = nums[ss];
+            return;
         }
-        return res;
+
+        int mid = ss + (se - ss)/ 2;
+        build(ss, mid, 2 * si + 1, nums);
+        build(mid + 1, se, 2 * si + 2, nums);
+
+        tree[si] = tree[2 * si + 1] ^ tree[2 * si + 2];
+    }
+
+    private int query(int qs, int qe, int ss, int se, int si) {
+        if(qs > se || qe < ss)
+            return 0;
+        if(qs <= ss && qe >= se)
+            return tree[si];
+        int mid = ss + (se - ss)/ 2;
+
+        return query(qs, qe, ss, mid, 2 * si + 1) ^ query(qs, qe, mid + 1, se, 2 * si + 2);
     }
 }
-//didnt read yet ..i remember doing it with tries too...will see later when we get time
+//didnt read yet ..i remember doing it with segment tree too...will see later when we get time
