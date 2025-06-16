c9ae01500c51b51238b07ff813c22fa648cadd36
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2025-06-17 00:08:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (22.18%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -1,15 +1,32 @@
+// class Solution {
+//     public int maximumDifference(int[] nums) {
+//         int max=-1;
+//         int len=nums.length;
+//         for(int i=len-1;i>=0;i--)
+//         {
+//             for(int j=i-1;j>=0;j--)
+//             {
+//                 int diff=nums[i]>nums[j]? nums[i]-nums[j]:-1;
+//                 max= diff>max ? diff:max;
+//             }
+//         }
+//         return max;
+//     }
+// }//mine O(n^2)
+
 class Solution {
     public int maximumDifference(int[] nums) {
-        int max=-1;
-        int len=nums.length;
-        for(int i=len-1;i>=0;i--)
-        {
-            for(int j=i-1;j>=0;j--)
-            {
-                int diff=nums[i]>nums[j]? nums[i]-nums[j]:-1;
-                max= diff>max ? diff:max;
+        int min = nums[0];
+        int maxDiff = -1;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > min) {
+                maxDiff = Math.max(maxDiff, nums[i] - min);
+            } else {
+                min = nums[i];
             }
         }
-        return max;
+        return maxDiff;
     }
 }
+//optimized O(n)
