df01092abff9a50d3043451e790756f5a81ec6ab
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2025-07-28 19:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (82.04%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -1,29 +1,48 @@
+// class Solution {
+//     public int countHillValley(int[] nums) {
+//         int ans=0;
+//         for(int i=1;i<nums.length-1;i++)
+//         {
+//             if(nums[i]!=nums[i+1]&&nums[i]!=nums[i-1])
+//             {
+//                 if((nums[i]>nums[i-1]&&nums[i]>nums[i+1])||
+//                 (nums[i]<nums[i-1]&&nums[i]<nums[i+1]))
+//                 {
+//                     ans++;
+//                 }
+//             }
+//             if(nums[i]==nums[i+1])
+//             {
+//                 int temp=i;
+//                 while(((i+1)<nums.length)&&nums[i]==nums[i+1])
+//                 i++;
+//                 if(((i+1)<nums.length)&&((nums[i]>nums[temp-1]&&nums[i]>nums[i+1])||
+//                 (nums[i]<nums[temp-1]&&nums[i]<nums[i+1])))
+//                 {
+//                     ans++;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+//self done but took a lil time like 10 to 15 min
+
 class Solution {
     public int countHillValley(int[] nums) {
-        int ans=0;
-        for(int i=1;i<nums.length-1;i++)
-        {
-            if(nums[i]!=nums[i+1]&&nums[i]!=nums[i-1])
-            {
-                if((nums[i]>nums[i-1]&&nums[i]>nums[i+1])||
-                (nums[i]<nums[i-1]&&nums[i]<nums[i+1]))
-                {
-                    ans++;
+        int count = 0;
+        int left = 0;
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                if ((nums[i] > nums[left] && nums[i] > nums[i + 1]) || 
+                    (nums[i] < nums[left] && nums[i] < nums[i + 1])) {
+                    count++;
                 }
-            }
-            if(nums[i]==nums[i+1])
-            {
-                int temp=i;
-                while(((i+1)<nums.length)&&nums[i]==nums[i+1])
-                i++;
-                if(((i+1)<nums.length)&&((nums[i]>nums[temp-1]&&nums[i]>nums[i+1])||
-                (nums[i]<nums[temp-1]&&nums[i]<nums[i+1])))
-                {
-                    ans++;
-                }
+                left = i;
             }
         }
-        return ans;
+        return count;
     }
 }
-//self done but took a lil time like 10 to 15 min
+//simplified version of my solution
